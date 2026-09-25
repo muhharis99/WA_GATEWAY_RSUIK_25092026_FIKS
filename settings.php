b4a1889f1f3d39eb06246f7582f6596fdb9728e1
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-25 20:02:48 +0700

    Seragamkan navbar settings.php

--- settings.php
+++ settings.php
@@ -44,25 +44,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body class="bg-body-tertiary">
     <nav class="navbar navbar-expand-lg bg-white border-bottom sticky-top">
         <div class="container py-2">
-            <a class="navbar-brand fw-bold text-success" href="index.php">
-                <?= e(APP_NAME) ?>
-            </a>
-
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#mainNavbar"
-            >
+            <a class="navbar-brand fw-bold text-success" href="index.php">Dokter Reminder RSU Islam Klaten</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="mainNavbar">
                 <div class="navbar-nav ms-auto">
                     <a class="nav-link" href="index.php">Dashboard</a>
                     <a class="nav-link" href="master.php">Master Data</a>
                     <a class="nav-link active fw-semibold" href="settings.php">Template</a>
                     <a class="nav-link" href="report.php">Report</a>
+                    <a class="nav-link" href="dokter_ijin.php">Dokter Ijin</a>
+                    <a class="nav-link" href="pindah_jam_praktek.php">Pindah Jam Praktek</a>
+                    <a class="nav-link" href="chat_dokter.php">Chat Dokter</a>
+                    <a class="nav-link" href="lab/index.php">LAB</a>
+                    <a class="nav-link" href="ijin/index.php">Ijin WA</a>
                 </div>
             </div>
         </div>
