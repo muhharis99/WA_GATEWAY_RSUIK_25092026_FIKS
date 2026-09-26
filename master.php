17eee6b3f342b006991dfe41a1b3bc41e8c3ba75
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-26 23:22:38 +0700

    refactor: remove chat doctor navigation

--- master.php
+++ master.php
@@ -160,8 +160,7 @@ $schedules = $pdo->query("
                     <a class="nav-link" href="report.php">Report</a>
                     <a class="nav-link" href="dokter_ijin.php">Dokter Ijin</a>
                     <a class="nav-link" href="pindah_jam_praktek.php">Pindah Jam Praktek</a>
-                    <a class="nav-link" href="chat_dokter.php">Chat Dokter</a>
-                    <a class="nav-link" href="lab/index.php">LAB</a>
+<a class="nav-link" href="lab/index.php">LAB</a>
                     <a class="nav-link" href="ijin/index.php">Ijin WA</a>
                 </div>
             </div>
