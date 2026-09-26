bb7d95e4fb862b76ce77fc60d3bc7b114381601e
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-27 00:12:32 +0700

    fix: clarify missing PHP dependency message

--- ijin/report_pdf.php
+++ ijin/report_pdf.php
@@ -10,7 +10,7 @@ $autoload = dirname(__DIR__) . '/vendor/autoload.php';
 
 if (!is_file($autoload)) {
     http_response_code(500);
-    exit('mPDF belum terpasang. Jalankan: composer install');
+    exit('Dependensi PHP belum terpasang. Jalankan `composer install` dari root project terlebih dahulu.');
 }
 
 require_once $autoload;
