51305e906067e4afa3831530309e8179c6a7f6f5
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-27 00:39:18 +0700

    ui: update navbar title for Ijin WA

--- ijin/index.php
+++ ijin/index.php
@@ -84,7 +84,7 @@ function h2($value): string
 
 <nav class="navbar navbar-expand-lg bg-white border-bottom sticky-top">
         <div class="container py-2">
-            <a class="navbar-brand fw-bold text-success" href="../index.php">Dokter Reminder RSU Islam Klaten</a>
+            <a class="navbar-brand fw-bold text-success" href="../index.php">Ijin WA</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
