bd00f183d9d8eaf59486ed3af5dfd05915344556
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-26 23:23:26 +0700

    refactor: remove chat doctor navigation

--- lab/report.php
+++ lab/report.php
@@ -90,8 +90,7 @@ $pdfQuery = http_build_query([
                     <a class="nav-link active fw-semibold" href="../report.php">Report</a>
                     <a class="nav-link" href="../dokter_ijin.php">Dokter Ijin</a>
                     <a class="nav-link" href="../pindah_jam_praktek.php">Pindah Jam Praktek</a>
-                    <a class="nav-link" href="../chat_dokter.php">Chat Dokter</a>
-                    <a class="nav-link" href="../lab/index.php">LAB</a>
+<a class="nav-link" href="../lab/index.php">LAB</a>
                     <a class="nav-link" href="../ijin/index.php">Ijin WA</a>
                 </div>
             </div>
