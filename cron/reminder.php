b4092142ecb421fa3aca334c9eb2c4fabc5bd543
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-26 23:43:08 +0700

    refactor: point PHP endpoints to app layer

--- cron/reminder.php
+++ cron/reminder.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-require_once dirname(__DIR__) . '/functions.php';
+require_once dirname(__DIR__) . '/app/Support/functions.php';
 $date = date('Y-m-d');
 ensureReminders($date);
 echo "Reminder siap untuk {$date}\n";
