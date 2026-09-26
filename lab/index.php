fb07edde04481b5e391ec4cb2133b74817bf9582
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-26 23:23:21 +0700

    refactor: remove chat doctor navigation

--- lab/index.php
+++ lab/index.php
@@ -16,8 +16,7 @@ function h($v):string{return htmlspecialchars((string)$v,ENT_QUOTES,'UTF-8');}
                     <a class="nav-link" href="../report.php">Report</a>
                     <a class="nav-link" href="../dokter_ijin.php">Dokter Ijin</a>
                     <a class="nav-link" href="../pindah_jam_praktek.php">Pindah Jam Praktek</a>
-                    <a class="nav-link" href="../chat_dokter.php">Chat Dokter</a>
-                    <a class="nav-link active fw-semibold" href="../lab/index.php">LAB</a>
+<a class="nav-link active fw-semibold" href="../lab/index.php">LAB</a>
                     <a class="nav-link" href="../ijin/index.php">Ijin WA</a>
                 </div>
             </div>
