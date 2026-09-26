e211002ea229096fac93f64e18ebd9fe0958cc9d
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-27 00:08:17 +0700

    fix: use RSU logo as browser favicon only

--- dokter_ijin.php
+++ dokter_ijin.php
@@ -94,11 +94,12 @@ try {
     <link href="https://cdn.datatables.net/v/bs5/dt-3.0.2/r-4.0.2/datatables.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/style.css">
+    <link rel="icon" type="image/png" href="https://rsuislamklaten.co.id/assets_front/images/logo-rsi-single.png">
 </head>
 <body class="bg-body-tertiary">
     <nav class="navbar navbar-expand-lg bg-white border-bottom sticky-top">
         <div class="container py-2">
-            <a class="navbar-brand fw-bold text-success d-flex align-items-center gap-2" href="index.php"><img src="https://rsuislamklaten.co.id/assets_front/images/logo-rsi-single.png" alt="RSU Islam Klaten" class="navbar-logo"> <span>Dokter Reminder RSU Islam Klaten</span></a>
+            <a class="navbar-brand fw-bold text-success" href="index.php">Dokter Reminder RSU Islam Klaten</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
