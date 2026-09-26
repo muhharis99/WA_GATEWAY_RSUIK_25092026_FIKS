728135bf290704f897a0b587b8ed4ed6d9b59615
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-27 00:30:42 +0700

    fix: preserve LAB whatsapp media workaround in MVC client

--- src/whatsapp/createClient.js
+++ src/whatsapp/createClient.js
@@ -1,12 +1,56 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const QRCode = require('qrcode');
 const WhatsAppLifecycle = require('./WhatsAppLifecycle');
 const { projectRoot } = require('../config/env');
 
+function patchWhatsAppWebMediaBug() {
+  const utilsPath = path.join(
+    projectRoot,
+    'node_modules',
+    'whatsapp-web.js',
+    'src',
+    'util',
+    'Injected',
+    'Utils.js'
+  );
+
+  if (!fs.existsSync(utilsPath)) {
+    return;
+  }
+
+  let source = fs.readFileSync(utilsPath, 'utf8');
+
+  if (source.includes('delete message.__x_id;')) {
+    return;
+  }
+
+  const markerRegex = /(\.\.\.extraOptions,\r?\n\s*\};)/;
+  if (!markerRegex.test(source)) {
+    return;
+  }
+
+  const backupPath = utilsPath + '.backup';
+  if (!fs.existsSync(backupPath)) {
+    fs.copyFileSync(utilsPath, backupPath);
+  }
+
+  source = source.replace(
+    markerRegex,
+    '$1\n        // Workaround whatsapp-web.js media regression (MediaData.__x_id)\n        delete message.__x_id;'
+  );
+
+  fs.writeFileSync(utilsPath, source, 'utf8');
+}
+
 function createWWebClient(clientId, authDir, options = {}) {
+  if (options.patchMediaBug) {
+    patchWhatsAppWebMediaBug();
+  }
+
   return new Client({
     authStrategy: new LocalAuth({
       clientId,
@@ -44,4 +88,4 @@ function createWhatsAppLifecycle(clientId, authDir, options = {}) {
   );
 }
 
-module.exports = { createWWebClient, createWhatsAppLifecycle };
+module.exports = { createWWebClient, createWhatsAppLifecycle, patchWhatsAppWebMediaBug };
